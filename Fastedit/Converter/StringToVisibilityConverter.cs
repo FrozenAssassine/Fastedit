968ce068db483ec9ff03e249e781f31eb070abe7
FrozenAssassine <Julius@skirsch.de>
2023-02-20 22:36:02 +0100

    This fixes #102

---
+++ Fastedit/Converter/StringToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml;
+
+namespace Fastedit.Converter
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, string language)
+        {
+            if (value == null)
+                return Visibility.Collapsed;
+
+            return (value as string).Length == 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
