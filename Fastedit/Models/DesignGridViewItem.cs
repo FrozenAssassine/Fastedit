82e9b3a9f79cad9e5572272002aa56a0b0b08587
Julius Kirsch <Julius@skirsch.de>
2024-01-12 17:40:33 +0100

    ref: own class file for DesigGridViewItem

---
+++ Fastedit/Models/DesignGridViewItem.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Media;
+
+namespace Fastedit.Models
+{
+    public class DesignGridViewItem
+    {
+        public Brush AppBackground { get; set; }
+        public Brush TextColor { get; set; }
+        public Brush LineNumberColor { get; set; }
+        public Brush TextBoxBackground { get; set; }
+        public Brush LineNumberBackground { get; set; }
+        public Brush TabPageBackground { get; set; }
+        public string DesignName { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
