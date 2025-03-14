205e47b8230c858e028a31d468fcda4991a9cecd
Julius Kirsch <Julius@skirsch.de>
2025-03-14 16:57:01 +0100

    feat: syntaxhighlighting manager wip

---
+++ Fastedit/Core/SyntaxHighlightingManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using TextControlBoxNS;
+
+namespace Fastedit.Core;
+
+internal class SyntaxHighlightingManager
+{
+    public SyntaxHighlightLanguage[] GetAllSyntaxHighlights()
+    {
+        SyntaxHighlightLanguage[] inbuild = TextControlBox.SyntaxHighlightings.Select(item => item.Value).ToArray();
+        SyntaxHighlightLanguage[] fromPlugins = [];
+        SyntaxHighlightLanguage[] fromApp = [];
+
+        return inbuild.Concat(fromPlugins).Concat(fromApp).ToArray();
+    }
+
+    public string GetIdentifier(SyntaxHighlightLanguage syntaxHighlighting)
+    {
+        return syntaxHighlighting.Author + syntaxHighlighting.Name;
+    }
+}
