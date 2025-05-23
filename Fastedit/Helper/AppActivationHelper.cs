1739023908428aaddaa30120a9ccdb433055b4cb
Julius Kirsch <Julius@skirsch.de>
2025-05-23 12:05:03 +0200

    fix: open files on app launch properly

--- Fastedit/Helper/AppActivationHelper.cs
+++ Fastedit/Helper/AppActivationHelper.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 using Fastedit.Core.Tab;
-using System;
-using System.Linq;
-using System.Diagnostics;
-using System.IO;
+using Microsoft.UI.Windowing;
 
 namespace Fastedit.Helper;
 
@@ -15,34 +12,15 @@ public class AppActivationHelper
     {
         if (appActivationArguments != null)
             return HandleFileActivation(tabView);
-
-        var args = Environment.GetCommandLineArgs();
-        if (args.Length == 0)
-            return false;
-        return HandleCommandLineActivation(tabView, args);
-    }
-
-    private static bool HandleCommandLineActivation(TabView tabView, string[] args)
-    {       
-        //no file, just open the app
-        if (args.Length == 1)
-            return true;
-
-        int successfullyOpened = 0;
-        if (args.Length >= 2)
-        {
-            string[] files = args.Skip(1).ToArray();
-            foreach (var file in files)
-            {
-                if (TabPageHelper.OpenAndShowFile(tabView, file, false))
-                    successfullyOpened++;
-            }
-        }
-        return successfullyOpened != 0;
+        return false;
     }
 
     private static bool HandleFileActivation(TabView tabView)
     {
+        var presenter = App.m_window.AppWindow.Presenter as OverlappedPresenter;
+        presenter.Minimize();
+        presenter.Restore();
+
         var file = appActivationArguments;
         if (file == null || file.Length == 0)
             return false;
